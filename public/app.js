0b01e4b034a8c7379eba8ac9c8ce479708b17c71
Dailey Kaze <dkaze@Daileys-MacBook-Air.local>
2021-05-24 23:29:47 -0400

    updated site

--- public/app.js
+++ public/app.js
@@ -27,6 +27,7 @@ const addBoxes = data => {
     data.map(img => {
 
         let node = document.createElement('div');
+        
         node.setAttribute("class", "image-box");
         body.appendChild(node);
 
@@ -35,5 +36,45 @@ const addBoxes = data => {
         imgNode.setAttribute("src", `./images/${img}`);
         imgNode.setAttribute("alt", `./images/${img}`);
         node.appendChild(imgNode);
+
+        node.addEventListener('click', e => handleOpenModal(e))
     })
+}
+
+const handleOpenModal = event => {
+    console.log('click event', event)
+
+    let modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+
+    let modalContent = document.createElement("div");
+    modalContent.setAttribute("class", "modal-content");
+
+    let closeBtn = document.createElement('i');
+    closeBtn.setAttribute("class", "close fas fa-times");
+    // closeBtn.textContent = 'X'
+
+    modalContent.appendChild(closeBtn);
+
+    let imageBox = document.createElement('div');
+    imageBox.setAttribute("class", "big-image-box");
+
+    let imgNode = document.createElement('img');
+    imgNode.setAttribute("class", "big-img");
+    imgNode.setAttribute("src", event.target.src);
+
+    imageBox.appendChild(imgNode);
+
+    let textNode = document.createElement('p');
+    textNode.textContent = event.target.alt;
+
+    imageBox.appendChild(textNode);
+
+    modalContent.appendChild(imageBox);
+
+    modal.appendChild(modalContent);
+
+    document.getElementById('main').appendChild(modal);
+
+    closeBtn.addEventListener("click", () => modal.remove());
 }
